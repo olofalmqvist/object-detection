c33d560490dc8e6bcf7544157727e4e5aa612417
Brainjuice7810 <mellgren_henrik@hotmail.com>
2018-05-28 20:37:40 +0200

    hej svejs

--- keras_layers/keras_layer_L2Normalization.py
+++ keras_layers/keras_layer_L2Normalization.py
@@ -45,7 +45,7 @@ class L2Normalization(Layer):
 
     def __init__(self, gamma_init=20, **kwargs):
         if K.image_dim_ordering() == 'tf':
-            self.axis = 3
+            self.dim = 3
         else:
             self.axis = 1
         self.gamma_init = gamma_init
@@ -53,13 +53,13 @@ class L2Normalization(Layer):
 
     def build(self, input_shape):
         self.input_spec = [InputSpec(shape=input_shape)]
-        gamma = self.gamma_init * np.ones((input_shape[self.axis],))
+        gamma = self.gamma_init * np.ones((input_shape[self.dim],))
         self.gamma = K.variable(gamma, name='{}_gamma'.format(self.name))
         self.trainable_weights = [self.gamma]
         super(L2Normalization, self).build(input_shape)
 
     def call(self, x, mask=None):
-        output = K.l2_normalize(x, self.axis)
+        output = K.l2_normalize(x, self.dim)
         return output * self.gamma
 
     def get_config(self):
